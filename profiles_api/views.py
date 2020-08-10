96a81adff93081275a5f5011b09d795e6bf6bff0
Nico Tambellini <ntambellini13@gmail.com>
2020-08-10 07:30:52 -0600

    Added login API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -98,3 +98,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+   """Handle creating user authentication tokens"""
+   renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
